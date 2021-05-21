a9f139fa69043fc3df64054a89b957e6a9c194e5
jseo9732 <79249376+jseo9732@users.noreply.github.com>
2021-05-21 23:10:44 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Contents from "./components/Contents";
+import Header from "./components/Header";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contents />      
+    </div>
+  );
+}
+
+export default App;
